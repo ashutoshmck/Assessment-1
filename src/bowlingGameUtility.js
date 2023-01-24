58acfcf5d413c294216dee70734acd65d4617a52
Ashutosh Senapati <ashutosh_senapati@mckinsey.com>
2023-01-24 11:29:39 +0530

    chore: lint tests and source

--- src/bowlingGameUtility.js
+++ src/bowlingGameUtility.js
@@ -1,78 +1,65 @@
-const maxFrames=10;
-const minRolls=12;
-const calculateBowlingScore=(rolls)=>
-{
-  if(!Array.isArray(rolls))
-  {
+const maxFrames = 10;
+const minRolls = 12;
+const calculateBowlingScore = (rolls) => {
+  if (!Array.isArray(rolls)) {
     throw new Error('Not an array');
   }
 
-  if(rolls.length<minRolls)
-  {
+  if (rolls.length < minRolls) {
     throw new Error('Invalid input');
   }
-  
-  let runningScore=0;
-  let numberOfFrames=0;
-  for(let i=0;i<rolls.length;)
-  {
-    if(!Number.isInteger(rolls[i])||(rolls[i]<0||rolls[i]>10))
-    {
+
+  let runningScore = 0;
+  let numberOfFrames = 0;
+  for (let i = 0; i < rolls.length;) {
+    if (!Number.isInteger(rolls[i]) || (rolls[i] < 0 || rolls[i] > 10)) {
       throw new Error('Invalid element in array');
     }
-    
-    if(numberOfFrames==maxFrames-1)
-    {
-      //open frame
-      if(rolls[i]+rolls[i+1]<10)
-      {
 
-        runningScore+=rolls[i]+rolls[i+1];
+    if (numberOfFrames === maxFrames - 1) {
+      //open frame
+      if (rolls[i] + rolls[i + 1] < 10) {
+
+        runningScore += rolls[i] + rolls[i + 1];
       }
-      else
-      {
-        runningScore+=rolls[i]+rolls[i+1]+rolls[i+2];
+      else {
+        runningScore += rolls[i] + rolls[i + 1] + rolls[i + 2];
       }
-      if((rolls.length-i)>3)
-      {
+      if ((rolls.length - i) > 3) {
         throw new Error('Invalid number of frames');
       }
       break;
     }
     //if spare or open
-    if(rolls[i]<10)
-    {
-      if(rolls[i]+rolls[i+1]==10){
-        runningScore+=rolls[i]+rolls[i+1]+rolls[i+2];
+    if (rolls[i] < 10) {
+      if (rolls[i] + rolls[i + 1] === 10) {
+        runningScore += rolls[i] + rolls[i + 1] + rolls[i + 2];
       }
-      else
-      {
-        runningScore+=rolls[i]+rolls[i+1];
+      else {
+        runningScore += rolls[i] + rolls[i + 1];
       }
       numberOfFrames++;
-      i+=2;
+      i += 2;
     }
     //strike
-    else if (rolls[i]==10)
-    {
-      runningScore+=rolls[i]+rolls[i+1]+rolls[i+2];
+    else if (rolls[i] === 10) {
+      runningScore += rolls[i] + rolls[i + 1] + rolls[i + 2];
       numberOfFrames++;
       i++;
     }
-    
+
   }
   return runningScore;
 };
-const findBestScore=(games)=>{
-  let bestScore=0;
+const findBestScore = (games) => {
+  let bestScore = 0;
   games.forEach((element) => {
-    let currentGameScore=calculateBowlingScore(element);
-    if(currentGameScore>bestScore)
-    {
-      bestScore=currentGameScore;
+    let currentGameScore = calculateBowlingScore(element);
+    if (currentGameScore > bestScore) {
+      bestScore = currentGameScore;
     }
-    
+
   });
   return bestScore;
 };
-module.exports={calculateBowlingScore,findBestScore};
+module.exports = { calculateBowlingScore, findBestScore };
--- test/bowlingGameUtility.test.js
+++ test/bowlingGameUtility.test.js
@@ -1,23 +1,57 @@
 const { calculateBowlingScore, findBestScore } = require('../src/bowlingGameUtility');
 
-describe('Bowling Game Utility',()=>{
-  describe('Bowling game score calculator',()=>{
-    it('should throw error when input is not array', ()=> {
-      expect(()=> {calculateBowlingScore('qwerty');}).toThrow('Not an array');
+describe('Bowling Game Utility', () => {
+  describe('Bowling game score calculator', () => {
+    it('should throw error when input is not array', () => {
+      expect(() => { calculateBowlingScore('qwerty'); }).toThrow('Not an array');
     });
-    it('should throw error when input is not array of numbers', ()=> {
-      expect(()=> {calculateBowlingScore(['qwerty',3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3]);}).toThrow('Invalid element in array');
+    it('should throw error when input is not array of numbers', () => {
+      expect(() => { calculateBowlingScore(['qwerty', 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3]); }).toThrow('Invalid element in array');
     });
-    it('should return wrong input when inputs don\'t add up', ()=> {
-      expect(()=> {calculateBowlingScore([8, 5, 3, 2]);}).toThrow('Invalid input');
+    it('should return wrong input when inputs don\'t add up', () => {
+      expect(() => { calculateBowlingScore([8, 5, 3, 2]); }).toThrow('Invalid input');
     });
-    it('should return total score of a game when input is correct', ()=> {
+    it('should return total score of a game when input is correct', () => {
       expect(calculateBowlingScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
     });
   });
-  describe('Maximum score calculator',()=>{
-    it('should return maximum score from multiple games', ()=> {
-      expect( findBestScore([[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6],[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]])).toBe(90);
+  describe('Calculates the score in the bowling game', () => {
+
+    it('should give 30 score when scored set1', () => {
+      const set1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10];
+
+      expect(calculateBowlingScore(set1)).toBe(30);
+    });
+
+    it('should give 90 when scored set2', () => {
+      const set2 = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+      expect(calculateBowlingScore(set2)).toBe(90);
+    });
+
+    it('should give 16 when scored set3', () => {
+      const set2 = [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      expect(calculateBowlingScore(set2)).toBe(16);
+    });
+    it('should give 136 when scored set3', () => {
+      const set2 = [10, 9, 1, 10, 4, 5, 3, 6, 4, 4, 1, 3, 10, 1, 9, 5, 5, 2];
+      expect(calculateBowlingScore(set2)).toBe(136);
+    });
+    it('should give 81 when scored set3', () => {
+      const set2 = [10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      expect(calculateBowlingScore(set2)).toBe(81);
+    });
+    it('should give 48 when scored set3', () => {
+      const set2 = [10, 5, 5, 9, 0];
+      expect(calculateBowlingScore(set2)).toBe(48);
+    });
+    it('should give 20 when scored set3', () => {
+      const set2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10];
+      expect(calculateBowlingScore(set2)).toBe(20);
+    });
+  });
+  describe('Maximum score calculator', () => {
+    it('should return maximum score from multiple games', () => {
+      expect(findBestScore([[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6], [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]])).toBe(90);
     });
   });
 });
